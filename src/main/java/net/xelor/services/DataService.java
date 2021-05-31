cf5504126d2178460652eb29229fd7efe0e6ac7a
FinalVoid <finalvoiddev@gmail.com>
2021-05-31 19:54:09 +0200

    Optimized imports

--- src/main/java/net/xelor/services/DataService.java
+++ src/main/java/net/xelor/services/DataService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public abstract class DataService {
     private static final Map<Class<? extends DataService>, DataService> dataServiceMap = new HashMap<>();
